687ccf2644554a539786dd66317361105bbe3c02
Pranav Dere <67577881+pranav99d@users.noreply.github.com>
2022-02-17 22:42:51 +0530

    Create Exercise15.sql

---
+++ JDBC/MySQL(Exercise)/Exercise15.sql
@@ -0,0 +1,7 @@
+/*Write a query to get 3 minimum salaries from Employees table.*/
+
+SELECT DISTINCT salary 
+FROM employees a 
+WHERE 3 >= (SELECT COUNT(DISTINCT salary) 
+FROM employees b 
+WHERE b.salary <= a.salary)
